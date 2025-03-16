335d8ee4d704a82ec464a164b3258f1597dd8655
Koresh06 <korets-24@mail.ru>
2025-03-16 21:47:49 +0300

    delete pre-commit

---
+++ src/infrastructure/repositories/sqlite/impl/users_repository_sqlite_impl.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select
+from src.domain.repositories.user_repository import UserRepositoryABC
+from src.infrastructure.repositories.sqlite.settings.db_helper import SQLiteDatabaseHelper
+from src.infrastructure.repositories.models.users import User
+from src.domain.entitys.user import UserModel
+
+
+class UsersRepositorySQLiteImpl(UserRepositoryABC):
+    def __init__(self, db_helper: SQLiteDatabaseHelper):
+        self.db_helper = db_helper
+
+    def create(self, user: UserModel) -> User:
+        with self.db_helper.get_session() as session:
+            create_user = User(
+                username=user.username,
+                email=user.email,
+                first_name=user.first_name,
+                last_name=user.last_name,
+            )
+            session.add(create_user)
+            session.commit()
+            session.refresh(create_user)
+            return create_user
+
+    def get_all(self) -> list[User]:
+        with self.db_helper.get_session() as session:
+            return session.scalars(select(User)).all()
+
+    def get_by_id(self, id: int) -> User:
+        with self.db_helper.get_session() as session:
+            return session.get(User, id)
+
+    def update(
+        self,
+        user: User,
+        user_update: UserModel,
+        partial: bool = False,
+    ) -> User:
+        with self.db_helper.get_session() as session:
+            user_in_db = session.get(User, user.id)
+            for key, value in user_update.model_dump(exclude_unset=partial).items():
+                setattr(user_in_db, key, value)
+            session.commit()
+            session.refresh(user_in_db)
+            return user
+
+    def delete(self, id: int) -> None:
+        with self.db_helper.get_session() as session:
+            user = session.get(User, id)
+            session.delete(user)
+            session.commit()
