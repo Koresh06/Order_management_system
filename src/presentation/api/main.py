9574f086ed2ab70875e4a98a7eaa9d859edb0f9b
Koresh06 <korets-24@mail.ru>
2025-03-17 23:21:57 +0300

    add routers

--- src/presentation/api/main.py
+++ src/presentation/api/main.py
@@ -2,16 +2,29 @@ from fastapi import FastAPI
 
 from src.application.containers.container import Container
 from src.presentation.api.users.router import router as users_router
+from src.presentation.api.orders.router import router as orders_router
+from src.presentation.api.roles.router import router as roles_router
+from src.presentation.api.products.router import router as products_router
 
 
 def create_app() -> FastAPI:
     container = Container()
-    container.wire(modules=["src.presentation.api.users.router"])
+    container.wire(
+        modules=[
+            "src.presentation.api.users.router",
+            "src.presentation.api.orders.router",
+            "src.presentation.api.roles.router",
+            "src.presentation.api.products.router",
+        ]
+    )
     app = FastAPI(
         title="API",
         description="API",
         version="1.0.0",
     )
+    app.include_router(roles_router)
     app.include_router(users_router)
+    app.include_router(products_router)
+    app.include_router(orders_router)
 
     return app
